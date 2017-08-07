694aa1f65b936fa463aea1c79e5c4143291483a9
Vinicius Batista da Fonseca Silva <viniciusbfs2012@gmail.com>
2017-08-06 22:23:43 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.acess_token,
-    access_token_secret: process.env.acess_token_secret
-};
