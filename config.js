c6719da09965b18fdc6ae9c2799bb4beb1e3210a
Vinicius Batista da Fonseca Silva <viniciusbfs2012@gmail.com>
2017-08-06 22:32:19 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.acess_token,
+    access_token_secret: process.env.acess_token_secret
+};
